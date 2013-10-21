4680e88705aa3e197d25efa99a58c0cfb8db8397
gnufabio <bonassi.fabio.94@gmail.com>
2013-10-21 23:42:22 +0200

    Added nex exercise, corrected K&R 2.5

--- Lab/20131024/2_numeri_deficienti.c
+++ Lab/20131024/2_numeri_deficienti.c
@@ -1,38 +1,53 @@
 /*
  *     Copyright 2013 gnufabio <bonassi.fabio.94@gmail.com>
- * 
+ *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
  *     (at your option) any later version.
- *     
+ *
  *     This program is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU General Public License for more details.
- *     
+ *
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  */
-
 #include <stdio.h>
+#define EXIT_SENTINEL -1
 
-/*
- * Write the function any(s1,s2) , which returns the first location in 
- * the string s1 where any character from the string s2 occurs, or −1 if s1 contains 
- * no characters from s2 . (The standard library function strpbrk does the same job 
- * but returns a pointer to the location.)
- */
-int any(const char *s1, const char *s2) {
+int main() {
+	int i, sigma, num;
 	
-	char s1c, s2c;
-	while((s1c = *(s1++)) != '\0') {
-		x = 0;
-		while((s2c = *(s2++)) != '\0') {
-			if(s1c == s2c) return index;
+	do {
+		printf("Please, enter a number (-1 to exit): ");
+		scanf("%d", &num);
+		
+		
+		if(num == EXIT_SENTINEL) break;
+		
+		if(num < 0) num = -num; /* consider abs(num) */
+		
+		sigma = 0;
+		
+		for(i = 1; i <= (num / 2); i++) {
+			if((num % i) == 0) {
+				sigma += i;
+			}
 		}
-	}
+		
+		if(sigma > num) {
+			printf("%d:\tDeficient number\n", num);
+		} else if(sigma == num) {
+			printf("%d:\tPerfect number\n", num);
+		} else if(sigma < num) {
+			printf("%d:\tAbundant number\n", num);
+		}
+		printf("\n");
+	} while(1);
 	
-	return -1;
+	getchar();
+	return 0;
 }
